ca71082f93d866f94804cc4b7a4ea2ecbcae2530
Spongebob <mistergd2281339@gmail.com>
2023-12-31 04:26:24 +0700

    Отчётик

--- Проекты/LB2.2/LB2.1.cpp
+++ Проекты/LB2.2/LB2.1.cpp
@@ -1,16 +1,21 @@
 ﻿#include <stdio.h>
 #include <locale.h>
+#include <limits.h>
 
 // Функция для нахождения разницы между двумя числами
 int findDifference(int a, int b) {
     return a > b ? a - b : b - a;
 }
 
+// Глобальные переменные для хранения подмножеств с минимальной разницей
+int minDifference = INT_MAX;
+int minSubset1[100], minSubset2[100];
+int minSubsetSize1 = 0, minSubsetSize2 = 0;
+
 // Функция для вывода подмножеств и их разницы
 void printSubsets(int set[], int subsets[], int n, int subsetSize) {
     int i, j;
     int sumSubset1 = 0, sumSubset2 = 0;
-    int minDifference = -1;
 
     // Вычисляем сумму каждого подмножества и находим минимальную разницу
     for (i = 0; i < n; i++) {
@@ -21,26 +26,22 @@ void printSubsets(int set[], int subsets[], int n, int subsetSize) {
             sumSubset2 += set[i];
         }
     }
-    minDifference = findDifference(sumSubset1, sumSubset2);
+    int difference = findDifference(sumSubset1, sumSubset2);
 
-    // Выводим подмножества и разницу их сумм
-    printf("Первое подмножество: ");
-    for (i = 0; i < n; i++) {
-        if (subsets[i] == 1) {
-            printf("%d ", set[i]);
+    // Если разница меньше минимальной, сохраняем подмножества
+    if (difference < minDifference) {
+        minDifference = difference;
+        minSubsetSize1 = 0;
+        minSubsetSize2 = 0;
+        for (i = 0; i < n; i++) {
+            if (subsets[i] == 1) {
+                minSubset1[minSubsetSize1++] = set[i];
+            }
+            else {
+                minSubset2[minSubsetSize2++] = set[i];
+            }
         }
     }
-    printf("\n");
-
-    printf("Второе подмножество: ");
-    for (i = 0; i < n; i++) {
-        if (subsets[i] == 0) {
-            printf("%d ", set[i]);
-        }
-    }
-    printf("\n");
-
-    printf("Разница: %d\n", minDifference);
 }
 
 // Функция для разбиения множества на непересекающиеся подмножества
@@ -81,6 +82,21 @@ int main() {
     int subsetSize = n / 2;
 
     divideIntoSubsets(set, subsets, subsetSize, n, 0, 0);
+
+    // Выводим подмножества с минимальной разницей
+    printf("Первое подмножество: ");
+    for (int i = 0; i < minSubsetSize1; i++) {
+        printf("%d ", minSubset1[i]);
+    }
+    printf("\n");
+
+    printf("Второе подмножество: ");
+    for (int i = 0; i < minSubsetSize2; i++) {
+        printf("%d ", minSubset2[i]);
+    }
+    printf("\n");
+
+    printf("Минимальная разница: %d\n", minDifference);
 
     return 0;
 }
